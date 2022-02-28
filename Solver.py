a206f931e39b9ac46a96179951d30000fca9f619
Maxime-Missichini <Themynder@gmail.com>
2022-02-28 08:36:24 +0100

    Writing Solver.py

--- Solver.py
+++ Solver.py
@@ -4,10 +4,10 @@ from Snake import Snake
 class Solver:
 
     class SearchNode:
-        def __init__(self, snake, priority):
+        def __init__(self, parent, snake, priority):
             self.snake = snake
             self.priority = priority
-            self.parent = None
+            self.parent = parent
 
         def compareTo(self, otherSearchNode):
             if self.priority < otherSearchNode.priority:
@@ -17,9 +17,22 @@ class Solver:
             else:
                 return 0
 
+        def isGoal(self):
+            return self.snake.score == (self.snake.windowSize*self.snake.windowSize)
+
     def __init__(self):
         self.initialSnake = Snake(-1, -1, -1, False)
         self.solution = None
 
     def AStar(self):
-        print("Astar")
+        startSearchN = Solver.SearchNode(None, self.initialSnake, 0)
+        nodeArray = []
+        nodeArray.insert(startSearchN)
+        current = nodeArray.pop()
+        while not current.isGoal():
+            self.addNext(nodeArray, current)
+
+    def addNext(self, nodeArray, current):
+        for next in current.snake.getNeighbors():
+            if not next.equals(current.parent.Snake):
+                nodeArray.insert(Solver.SearchNode(current, next, current.priority + 1))
