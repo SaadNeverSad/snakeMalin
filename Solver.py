c68c85511470eba0816180e180fbf8efa425870b
Maxime-Missichini <Themynder@gmail.com>
2022-02-28 08:17:17 +0100

    Clean code by adding classes

---
+++ Solver.py
@@ -0,0 +1,25 @@
+from Snake import Snake
+
+
+class Solver:
+
+    class SearchNode:
+        def __init__(self, snake, priority):
+            self.snake = snake
+            self.priority = priority
+            self.parent = None
+
+        def compareTo(self, otherSearchNode):
+            if self.priority < otherSearchNode.priority:
+                return -1
+            elif self.priority > otherSearchNode.priority:
+                return 1
+            else:
+                return 0
+
+    def __init__(self):
+        self.initialSnake = Snake(-1, -1, -1, False)
+        self.solution = None
+
+    def AStar(self):
+        print("Astar")
